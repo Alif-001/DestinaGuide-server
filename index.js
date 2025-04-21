f2b342d9843c0084894c7b86786b05c601197af8
Md. Omor Faruk Alif <87005321+Alif-001@users.noreply.github.com>
2025-04-21 19:21:57 +0600

    create tourist-spots get and post route

--- index.js
+++ index.js
@@ -6,13 +6,73 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+//mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// const uri = process.env.MONGODB_URI;
+
+// connect to mongodb locally
+const uri = "mongodb://localhost:27017/";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Connect to the database and access its collection
+    const database = client.db("destinaGuide");
+    const touristSpotsCollection = database.collection("touristSpots");
+
+    /************************/
+          // routes
+    /***********************/
+
+    // Get all tourist spots
+    app.get("/tourist-spots", async (req, res) => {
+      const cursor = touristSpotsCollection.find({});
+      const touristSpots = await cursor.toArray();
+      res.json(touristSpots);
+    });
+
+    //add tourist spot
+    app.post("/tourist-spots", async (req, res) => {
+      const touristSpot = {
+        ...req.body,
+        createdAt: new Date(),
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      };
+      const result = await touristSpotsCollection.insertOne(touristSpot);
+      res.status(201).json(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port} ✨`);
 });
